3ace39145b59d1d7f2e088da43681f55e45f0f6f
manupeisa <134011558+manupeisa@users.noreply.github.com>
2026-04-07 21:09:06 +0000

    Agrego ej1.c

---
+++ ej1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+typedef struct {
+    nombre char[];
+    vida int;
+    ataque double;
+    defensa double;
+} mounstro_t;
+
+mounstro_t m1 = {"Juan", 1000, 2600, 3000};
+mounstro_t m2 ={"Messi", 2000, 4000, 6000};
+int main(){
+
+    return 0;
+}
