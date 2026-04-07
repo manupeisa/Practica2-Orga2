8cf336e62eb51ddcc90abdc1b02c3736d885ddf3
manupeisa <134011558+manupeisa@users.noreply.github.com>
2026-04-07 22:16:15 +0000

    Agrego el ej 3

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -pedantic
-TARGETS = ej1 ej2 
+TARGETS = ej1 ej2 ej3
 #TARGETS = malloc
 all: $(TARGETS)
 
---
+++ ej3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int x = 42;
+    int *p = &x;
+
+    printf("Direccion de x: %p Valor: %d\n", (void*) &x, x);
+    printf("Direccion de p: %p Valor: %p\n", (void*) &p, (void*) p);
+    printf("Valor de lo que apunta p: %d\n", *p);
+}
+
+/*
+Ej 3
+La diferencia entre x y p es que x es la varaible la cual almacena un valor y p
+unicamente es un puntero que apunta a la direcion de memoria de x
+La diferencia entre x y &x es que x es la varaible y &x es la direccion de memoria 
+donde esta esta guardada 
+Lo que cambia entre p y *p es que  p es el valor de p y *p es el valor al caul apunta p
+
+Cuando se ejecute va a imprimir :
+La direccion de memoria de X , El valor de x = 42 
+La direccion donde esta el puntero y donde esta la memoria de X 
+El valor el cual eesta apuntando p
+*/
+
