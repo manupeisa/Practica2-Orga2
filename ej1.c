1f9fcbd0fd75ea0c761b1923ae447318a9b3f036
manupeisa <134011558+manupeisa@users.noreply.github.com>
2026-04-07 21:33:28 +0000

    Corrijo ej1.c

--- ej1.c
+++ ej1.c
@@ -1,18 +1,23 @@
 #include <stdio.h> 
 
 typedef struct {
-    char[] nombre;
+    char[20] nombre;
     int vida;
     double ataque;
     double defensa;
 } mounstro_t;
 
-mounstro_t mounstros[] = {
-    {"Juan", 1000, 2600, 3000},
-    {"Messi", 2000, 4000, 6000},
-    {"Manu", 9999, 90000, 100000}
-};
 
-printf("Imprimo el mounstro y su vida : %s%d\n", mounstros[0].nombre, mounstros[0].vida);
-printf("Imprimo el mounstro y su vida : %s%d\n", mounstros[1].nombre, mounstros[1].vida);
-printf("Imprimo el mounstro y su vida : %s%d\n", mounstros[2].nombre, mounstros[2].vida);
+int main(){
+    mounstro_t mounstros[] = {
+        {"Juan", 1000, 2600, 3000},
+        {"Messi", 2000, 4000, 6000},
+        {"Manu", 9999, 90000, 100000}
+    };
+
+    printf("Imprimo el mounstro y su vida : %s %d\n", mounstros[0].nombre, mounstros[0].vida);
+    printf("Imprimo el mounstro y su vida : %s %d\n", mounstros[1].nombre, mounstros[1].vida);
+    printf("Imprimo el mounstro y su vida : %s %d\n", mounstros[2].nombre, mounstros[2].vida);
+
+    return 0;
+}
