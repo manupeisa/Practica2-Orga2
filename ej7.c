fd4294333d5e110085532548ef511780badf8d02
ManuPeisa <manupeisa@gmail.com>
2026-04-08 19:14:53 -0300

    Modifico mi ej7

--- ej7.c
+++ ej7.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main(){
     int x = 10, y =20;
-    swap(&x, &y);
+    swap(x, y);
     printf("X : %d Y: %d", x, y);
 
     return 0;
@@ -16,6 +16,8 @@ int main(){
 
 
 /*
-No hay diferencia si nuestro swap lo poenmos con el void en el caso de que lo usemos dentro de nuestro main con varaibles del main,
-pero si usamos unicamente el swap no nos va a devolver nada 
+Ahora que cambie las variables a int a y int b mis valores no cambian por lo que no sirve mi swap
+
+No se modifican ya que lo que hace mi swap es darle las copias de x e y y dentro del swap si se modifican los valores de las copias 
+pero no modifica el valor real de x e y ya que solo le paso copias no su direccion de memoria 
 */
