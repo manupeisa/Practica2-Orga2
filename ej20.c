a46060c4868221eb6e58778973a8370218b78493
ManuPeisa <manupeisa@gmail.com>
2026-04-10 20:13:37 -0300

    Explicacion codigo ej20 completo

--- ej20.c
+++ ej20.c
@@ -26,7 +26,10 @@ teniendo en cuenta que mis columnas van a ser de largo VIDEO_COLS  osea qeu voy 
 Lo que hace p[f][c].c = ’ ’ es en el puntero p que es del tripo sruct ca es en el dato c el cual es caracter asignarle el valor vacio osea
 ' '  y como nos explicaron el formato es de 2 bytes lo cual el primero es el caracter el cual todos seran ' ' y el segundo byte el cual sera
 el color del caracter los cuales todos tengran el color 0x10
+el proceso de desrefeneciacion lo que es es que mas que poner *(*(p + f) +c) == p[f][c] lo que es exactamente lo mismo y al hacer 
+p[f][c].c lo que hacemos es acceder al primer byte el cual es caracteres 
 
-
-Si en vez de ca(*p)[VIDEO COLS] hubi ́eramos declarado ca** p nuestro puntero p no tendria el formato 
+Si en vez de ca(*p)[VIDEO COLS] hubi ́eramos declarado ca** p nuestro puntero p no tendria el formato pedidio y se roperia ya que lo que diria 
+es que p es una puntero el cual apunta a un puntero de f .Pero el buffer de video no está armado así. No contiene 50 punteros a filas.
+ Contiene todos los ca uno atrás de otro en memoria contigua
 */
